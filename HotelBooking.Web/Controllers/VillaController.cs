7eb1df6fee785c0382b1118e13fd5dde04751654
Najmul Islam Naeem <csenajmulislamnaeem@gmail.com>
2025-01-20 00:55:49 +0600

    Villa is inserted to MS SQL Server and data are fetches from database.

---
+++ HotelBooking.Web/Controllers/VillaController.cs
@@ -0,0 +1,43 @@
+﻿using HotelBooking.Domain.Entities;
+using HotelBooking.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Elfie.Serialization;
+
+namespace HotelBooking.Web.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        
+        public VillaController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {  
+            var villas = _db.Villas.ToList();
+            return View(villas);
+        }
+
+        public IActionResult Create()
+        {  
+          
+            return View( );
+        }
+
+        [HttpPost]
+        public IActionResult Create( Villa obj)
+        {
+            _db.Villas.Add(obj);
+            _db.SaveChanges();
+             return  RedirectToAction("Index");
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+
+    }
+}
